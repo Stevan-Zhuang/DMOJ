9b5e2a2fdb5b03ba4c2fd0000d2a19524cc44169
Stevan Zhuang <Stevan.Zhuang@gmail.com>
2020-09-17 19:50:48 -0700

    Update CCC '00 S5 - Sheep and Coyotes.py

--- CCC/CCC '00 S5 - Sheep and Coyotes.py
+++ CCC/CCC '00 S5 - Sheep and Coyotes.py
@@ -1,3 +1,39 @@
-sheeps = [(float(input()), float(input())) for sheep in range(int(input()))]
-for sheep in set(sorted(((sheep[0] - point) ** 2 + sheep[1] ** 2, sheep) for sheep in sheeps)[0][1] for point in (x * 0.05 for x in range(20001))):
-    print("The sheep at ({:.2f}, {:.2f}) might be eaten.".format(sheep[0], sheep[1]))
+from decimal import Decimal
+from collections import namedtuple
+input = __import__('sys').stdin.readline
+Point = namedtuple('Point', ['x', 'y'])
+
+num_sheeps = int(input())
+sheeps = [Point(Decimal(input()), Decimal(input()))
+          for idx in range(num_sheeps)]
+valid = [True] * num_sheeps
+
+for idx1 in range(num_sheeps):
+    valid_left, valid_right = 0, 1000
+    for idx2 in range(num_sheeps):
+        if idx1 == idx2:
+            continue
+        sheep1, sheep2 = sheeps[idx1], sheeps[idx2]
+        if valid[idx1] and valid[idx2]:
+            middle = Point((sheep1.x + sheep2.x) / 2, (sheep1.y + sheep2.y) / 2)
+            rise, run = (sheep2.y - sheep1.y), (sheep2.x - sheep1.x)
+            if run == 0:
+                if rise < 0: valid[idx1] = False
+                if rise > 0: valid[idx2] = False
+                continue
+            elif rise == 0:
+                x_inter = middle.x
+            else:
+                slope = rise / run
+                perp_slope =  -1 / slope
+                x_inter = -middle.y / perp_slope + middle.x
+
+            if run < 0: valid_left = max(x_inter, valid_left)
+            if run > 0: valid_right = min(x_inter, valid_right)
+
+    if valid_left > valid_right:
+        valid[idx1] = False
+
+for idx in range(num_sheeps):
+    if valid[idx]:
+        print(f"The sheep at ({sheeps[idx].x:.2f}, {sheeps[idx].y:.2f}) might be eaten.")
