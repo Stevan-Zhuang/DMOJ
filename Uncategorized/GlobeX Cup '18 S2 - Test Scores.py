be0c995f2ca46d649b21f9d10a2c959aed615f84
Stevan Zhuang <Stevan.Zhuang@gmail.com>
2021-01-28 21:44:47 -0800

    Update GlobeX Cup '18 S2 - Test Scores.py

--- Uncategorized/GlobeX Cup '18 S2 - Test Scores.py
+++ Uncategorized/GlobeX Cup '18 S2 - Test Scores.py
@@ -7,7 +7,7 @@ total_time = 0
 total_mark = 0
 
 arr = [tuple(int(data) for data in input().split())
-             for _ in range(num_tests)]
+       for _ in range(num_tests)]
 total_mark += sum(mark for mark, _ in arr)
 arr = sorted((time, mark_total - mark) for mark, time in arr)
 
