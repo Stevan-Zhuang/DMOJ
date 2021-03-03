c1c0084a358c6bce16ee506e3a52761baa6e9e8d
Stevan Zhuang <Stevan.Zhuang@gmail.com>
2021-03-03 15:47:21 -0800

    Update CCC '21 S3 - Lunch Concert.py

--- CCC/CCC '21 S3 - Lunch Concert.py
+++ CCC/CCC '21 S3 - Lunch Concert.py
@@ -1,29 +1,37 @@
+input = __import__('sys').stdin.readline
+
 n_people = int(input())
-data = [[int(data) for data in input().split()]
-        for _ in range(n_people)]
+
+walk_left = 0
+walk_right = 0
+cur_time = 0
+prev_pos = 0
+
 people = []
-for p, w, d in data:
-    people.append((p - d, w, 1))
+for _ in range(n_people):
+    p, w, d = [int(data) for data in input().split()]
     people.append((p + d, w, 0))
+    if p - d > 0:
+        people.append((p - d, w, 1))
+        cur_time += (p - d) * w
+        walk_right += w
+
 people.sort()
-del data
 
 best_time = float('inf')
-for cur, (pos, walk, left) in enumerate(people):
-    if cur == 0:
-        total_walk_left = 0
-        total_walk_right = sum(w for p, w, l in people if l and pos < p)
+for pos, walk, left in people:
+    best_time = min(best_time, cur_time)
 
-        cur_time = sum((p - pos) * w for p, w, l in people if l)
-        best_time = min(best_time, cur_time)
-    else:
-        dist = (people[cur][0] - people[cur - 1][0])
-        
-        cur_time += dist * total_walk_left
-        cur_time -= dist * total_walk_right
-        best_time = min(best_time, cur_time)
+    dist = pos - prev_pos
+    
+    cur_time += dist * walk_left
+    cur_time -= dist * walk_right
+    best_time = min(best_time, cur_time)
 
-        if not left: total_walk_left += walk
-        if left: total_walk_right -= walk
+    prev_pos = pos
+    if not left:
+        walk_left += walk
+    if left:
+        walk_right -= walk
 
 print(best_time)
